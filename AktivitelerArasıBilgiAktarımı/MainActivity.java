52308dd12cb066efbb0fc111c09fdf02722ad5ca
tunahan burak dirlik <87035514+tburakdirlik@users.noreply.github.com>
2021-08-12 19:36:27 +0300

    Add files via upload

---
+++ AktivitelerArasıBilgiAktarımı/MainActivity.java
@@ -0,0 +1,38 @@
+package com.tbd.multipleactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    String userName;
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        userName = "";
+        editText = findViewById(R.id.editTextText);
+
+    }
+
+    public void changeActivity (View view){
+
+
+        userName = editText.getText().toString();
+
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+        intent.putExtra("userInput", userName);
+
+        startActivity(intent);
+
+
+    }
+}
